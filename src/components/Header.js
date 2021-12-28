56f54d7017b3474d6dacc5e388f1ca955b66d7c9
Anton <grishin.ant.a@gmail.com>
2021-12-28 12:35:42 +0500

    добавлены ссылки на репозиторий

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ function Header() {
   return (
     <nav className="cyan">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           Fortnite Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#l">Repo</a>
+            <a href="https://github.com/AnthonyGr/fortnite-shop--udemy-course--react" target="_blank" rel="noreferrer">
+              Repo
+            </a>
           </li>
         </ul>
       </div>
